df2d19c43c6e7e433b0d68c15991d0e21b041b97
wanglu0421 <lu.wang_chengdu@dragonplus.com>
2020-06-29 00:41:20 +0800

    JSON2CLASS

---
+++ wznglu/Assets/JsonNet/JsonNetSample/JsonNetSample.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+public class WorldConfig
+{
+    public Int32 WorldId { get; set; }
+    public String WorldName { get; set; }
+}
+
+public class JsonNetSample : MonoBehaviour
+{
+    public Text Output;
+
+    void Start()
+    {
+        WriteLine("\nStart!\n");
+        Jsontext();
+        WriteLine("\nDone!");
+    }
+    private void Jsontext()
+    {
+        List<WorldConfig> WorldConfigs;
+        TextAsset ta = Resources.Load<TextAsset>("world");
+        string jsStr = ta.text;
+        Hashtable table = JsonConvert.DeserializeObject<Hashtable>(jsStr);
+        object tempObj = table["worlds"];
+        String tempObjStr = JsonConvert.SerializeObject(tempObj);
+        WorldConfigs = JsonConvert.DeserializeObject<List<WorldConfig>>(tempObjStr);
+
+        for (int i = 0; i < WorldConfigs.Count; i++)
+        {
+            WriteLine(WorldConfigs[i].WorldName);
+        }
+    }
+
+  void WriteLine(string msg)
+    {
+        Output.text = Output.text + msg + "\n";
+    }
+}
